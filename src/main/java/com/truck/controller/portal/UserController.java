f10e5dafde951e43f17e190b97ddb116faeed32a
zhangmingming <15026677294@163.com>
2018-06-01 14:13:43 +0700

    shijian

--- src/main/java/com/truck/controller/portal/UserController.java
+++ src/main/java/com/truck/controller/portal/UserController.java
@@ -102,8 +102,8 @@ public class UserController {
     public ServerResponse<User> getUserInfo(HttpSession session){
         Thread thread = Thread.currentThread();
         try {
-            Thread.sleep(10000);
             logger.info("沉睡10");
+            Thread.sleep(10000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
